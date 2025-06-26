05029d7083fa2af1cdd5808f6116ca1d863ecb89
lilacandolle <lila.cassan@gmail.com>
2025-06-26 17:06:32 +0200

    évolution inteannuelle des clusters

--- R/functions/clustering_functions.R
+++ R/functions/clustering_functions.R
@@ -80,74 +80,16 @@ fit_regression_multi <- function(data, vars, trend = TRUE) {
 #' @param clcolname Name of the column containing the cluster number
 #' @param sub_dir Subdirectory where to save the plots
 #' @param nom Name of the clustering method
-#' @param windu Name of the column containing the u component of the wind
-#' @param windv Name of the column containing the v component of the wind
-#' @param vars Names of the columns containing the variables to plot
-#' @param varnames Names of the variables to plot
 #' @param savefig Logical indicating whether to save the plots
+#' @param vars_dict A named list containing the variables to plot and their labels
+#' @param windf_col Name of the column containing wind speed
+#' @param windd_col Name of the column containing wind direction
 #' 
 #' @return NULL
 #' 
 #' @example plot_clustering_results(mean_dayanddn, 'gmm_cluster', 'GMMboxplots_roses', 'GMM')
-old_plot_clustering_results <- function(data, clcolname, sub_dir, nom, windu = windu, windv = windv,
-                                    vars = c('t2m_ano', 'relative_humidity_ano', 'surfacepressure_ano', 'ssrd_ano', 'windu', 'windv', 'wd', 'ws', 'totalprecipitation'),
-                                    varnames = c("Anomalies de température_a_2m", "Anomalies de humidité_relative", "Anomalies de Surface_pressure", "Anomalies de Solar_surface_radiation_downwards", 'vent_u', 'vent_v', 'wind dir', 'wind speed', 'Précipitations totales'),
-                                    savefig = TRUE) {
-  
-  # Boxplots
-  cluster_count <- as.data.frame(table(data[[clcolname]]))
-  names(cluster_count) <- c("cluster", "count")
-  cluster_count$label <- paste0("Cluster ", cluster_count$cluster, "\n(N=", cluster_count$count, ")")
-  data$cluster_label <- factor(data[[clcolname]], 
-                               levels = cluster_count$cluster, 
-                               labels = cluster_count$label)
-  for (i in 1:length(vars)) {
-    plot_box <- ggplot(data, aes(x = cluster_label, y = !!sym(vars[i]), fill = as.factor(!!sym(clcolname)))) +
-      geom_boxplot() +
-      labs(title = paste(varnames[i], "par", clcolname), 
-           x = clcolname, 
-           y = varnames[i],
-           fill = "Cluster") +
-      theme_minimal() +
-      scale_fill_viridis_d()
-    
-    # Afficher le plot à l'écran
-    print(plot_box)
-    
-    # Sauvegarder le plot si savefig est TRUE
-    if (savefig) {
-      ggsave(file.path(figpath, sub_dir, paste("boxplot_ano_", varnames[i], "_clustering_", nom, ".png", sep = '')), 
-             width = 10, height = 6, bg = "white")
-    }
-  }
-  
-  # Wind rose
-  plot_rose <- ggplot(data, aes(x = !!sym("wd"), y = !!sym("ws"), color = as.factor(!!sym(clcolname)))) +
-    geom_point() +
-    coord_polar(start = 0) +  
-    scale_x_continuous(limits = c(0, 360), breaks = seq(0, 360, by = 45), labels = c("N", "NE", "E", "SE", "S", "SO", "O", "NO", "N")) +
-    labs(x = "Direction", y = "Vitesse du vent", color = "Cluster") +
-    theme_minimal() +
-    facet_wrap(as.formula(paste("~", clcolname))) +
-    scale_color_viridis_d()
-  
-  # Afficher le wind rose à l'écran
-  print(plot_rose)
-  
-  # Sauvegarder le wind rose si savefig est TRUE
-  if (savefig) {
-    ggsave(file.path(figpath, sub_dir, paste(nom, "_rose_par_cluster.png", sep = '')), 
-           width = 10, height = 6, bg = "white")
-  }
-  cluster_count <- table(data[[clcolname]])
-  
-  print(cluster_count)
-}
-
-
 #### version OK
-plot_clustering_results <- function(data, clcolname, sub_dir, nom, 
-                                    savefig = savefig,
+plot_clustering_results <- function(data, clcolname, sub_dir, nom,
                                     vars_dict = list(
                                       t2m_ano = "Anomalies de température à 2m",
                                       relative_humidity_ano = "Anomalies de humidité_relative",
